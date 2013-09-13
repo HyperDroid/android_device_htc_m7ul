58aae5739bafcb956220c5129b5a6f59505e5d46
kristofpetho <kristof.petho@gmail.com>
2013-09-13 19:38:02 +0200

    aoxp: 3.17 bringup
    Change-Id: Idb69ba720c84bbbf8586fe9223d8cc754dd2d9ed

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JSS15Q BUILD_FINGERPRINT="htc/htc_europe/m7:4.3/JSS15J/245412.2:user/release-keys" PRIVATE_BUILD_DESC="3.17.401.2 CL245412 release-keys "
 
 PRODUCT_NAME := cm_m7ul
 PRODUCT_DEVICE := m7ul
